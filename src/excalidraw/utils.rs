ef449b9c0c2eb2cd92a802fd8aed02e5a46cef16
nkxxll <niki.nice1203@gmail.com>
2025-06-11 17:55:09 +0200

    test the index func

--- src/excalidraw/utils.rs
+++ src/excalidraw/utils.rs
@@ -2,27 +2,43 @@ use rand::Rng;
 
 use super::structures::{ExcalidrawFile, Rectangle};
 
+pub struct Generator {
+    pub index: usize,
+}
+
+impl Default for Generator {
+    fn default() -> Self {
+        Self {
+            index: 1,
+        }
+    }
+}
+
+impl Generator {
+    pub fn small_rectangle(self, x: f64, y: f64) -> Rectangle {
+        Rectangle {
+            x,
+            y,
+            index: generate_index(self.index),
+            ..Default::default()
+        }
+    }
+
+    pub fn big_rectangle(self, x: f64, y: f64) -> Rectangle {
+        Rectangle {
+            x,
+            y,
+            width: 100.0,
+            height: 100.0,
+            index: generate_index(self.index),
+            ..Default::default()
+        }
+    }
+}
+
 pub fn simple_drawing(elements: Vec<Rectangle>) -> ExcalidrawFile {
     ExcalidrawFile {
         elements,
-        ..Default::default()
-    }
-}
-
-pub fn small_rectangle(x: f64, y: f64) -> Rectangle {
-    Rectangle {
-        x,
-        y,
-        ..Default::default()
-    }
-}
-
-pub fn big_rectangle(x: f64, y: f64) -> Rectangle {
-    Rectangle {
-        x,
-        y,
-        width: 100.0,
-        height: 100.0,
         ..Default::default()
     }
 }
@@ -41,7 +57,7 @@ pub(crate) fn rand_element_id() -> String {
 }
 
 pub(crate) fn generate_index(index: usize) -> String {
-    let chars = "0123456789abcdefghijklmnopqrstuvw";
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyz";
     if index > chars.len() * chars.len() {
         panic!("index too high")
     }
@@ -49,4 +65,26 @@ pub(crate) fn generate_index(index: usize) -> String {
     let first = chars.chars().nth(index / chars.len()).unwrap();
     // rest has to be build like alphanumeric field
     format!("b{}{}", first, last)
+}
+
+#[cfg(test)]
+mod test {
+    use super::generate_index;
+
+    #[test]
+    fn test_index_gen() {
+        let exp1 = "b00";
+        let exp2 = "b01";
+        let exp3 = "b11";
+        let exp4 = "bzz";
+        let res1 = generate_index(0);
+        let res2 = generate_index(1);
+        let res3 = generate_index(37);
+        let res4 = generate_index(36 * 36 - 1);
+
+        assert_eq!(exp1, res1);
+        assert_eq!(exp2, res2);
+        assert_eq!(exp3, res3);
+        assert_eq!(exp4, res4);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod excalidraw;
-use excalidraw::utils::{self, simple_drawing};
+use excalidraw::utils::{simple_drawing, Generator};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let elems = vec![utils::big_rectangle(100.0, 100.0)];
+    let g = Generator::default();
+    let elems = vec![g.big_rectangle(100.0, 100.0)];
     let file = simple_drawing(elems);
     let res = serde_json::to_string(&file);
     println!("{}", res.unwrap());
